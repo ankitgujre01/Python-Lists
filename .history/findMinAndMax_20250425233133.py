4bd1d197a56705429252b9d0e863402e75c96379
ankitgujre <kumarankit65494@gmail.com>
2025-04-25 23:32:44 +0530

    add code

---
+++ .history/findMinAndMax_20250425233133.py
@@ -0,0 +1,27 @@
+def find_min_max(arr):
+
+    if not arr:
+        return None
+
+    min_val = arr[0]
+    max_val = arr[0]
+
+    for num in arr:
+        if num < min_val:
+            min_val = num
+        if num > max_val:
+            max_val = num
+
+    return (min_val, max_val)
+
+arr1 = [3, 2, 1, 56, 10000, 167]
+result1 = find_min_max(arr1)
+print(result1)
+
+arr2 = [1, 345, 234, 21, 56789]
+result2 = find_min_max(arr2)
+print(result2)  # Output: (1, 56789)
+
+arr3 = [56789]
+result3 = find_min_max(arr3)
+print(result3)  # Output: (56789, 56789)
